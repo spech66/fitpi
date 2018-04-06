58aab3869cdbc2059fa719f75353be65f021e7b6
Pech <PECH09@arvato-logistics.de>
2018-04-06 15:00:17 +0200

    filereader and demo data

--- fitpi-backend/models/measurement.js
+++ fitpi-backend/models/measurement.js
@@ -1,7 +1,25 @@
-exports.all = function(cb) {
-    var list = [
-        { date: '20180405', weight: 100 },
-        ];
+var fs = require('fs');
+//var readline = require('readline');
+// Read files without readline+async+promises => this is a local fun project. no large files expected...
 
-    cb(null, list);
+exports.all = function(cb) {
+    fs.readFile("./fitpi-data/measurement.txt", 'utf8', function(err, data) {
+        if (err) throw err;
+        //console.log(data)
+
+        var list = [];
+
+        data.toString().replace(/\r\n/g, '\n').split('\n').forEach(function (line) {
+            item = {};
+
+            text = line.split(';');
+
+            item.date = text[0];
+            item.weight = text[1];
+
+            list.push(item);
+        })
+        
+        cb(null, list);
+      });
 }
--- fitpi-frontend/src/App.js
+++ fitpi-frontend/src/App.js
@@ -24,10 +24,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Test</p>
 
         <h1>Motivations</h1>
         {this.state.motivations.map(motivation =>
-          <div key={motivation.id}>{motivation.text}</div>
+          <div>{motivation.text} <em>{motivation.source}</em> {motivation.image}</div>
         )}
 
       </div>
